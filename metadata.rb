92a209e9ef24ec54e3f8e4f62bbd8e3eaff5577c
Eric Herot <eric.github@herot.com>
2015-11-19 19:32:43 -0500

    Version bump 3.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'MIT'
 description      'Installs/Configures mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 
 supports         'ubuntu', '>= 14.04'
 supports         'centos', '>= 6.6'
